c671d86ab0aabcafc69eceb4f298e3953c8a1dc8
kim_chi_96 <numanaftab5432@gmail.com>
2026-02-18 08:33:02 +0000

    Bringup for alphadroid

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,7 +153,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2023-05-01
 
 # Sepolicy
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/alpha/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/sony/timekeep/sepolicy/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
